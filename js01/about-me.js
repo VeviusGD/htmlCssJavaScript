36bdbe4a728a215e78262ac37be8b0bb35fe3f5e
VeviusGD <mc_mphillips27@mcjvs2.org>
2025-10-23 09:07:29 -0400

    javascript 1

---
+++ js01/about-me.js
@@ -0,0 +1,14 @@
+// Log messages to the browsers developer console
+console.log("Page script has started running.");
+
+// Show a simple alert box to welcome visitors when the page loads
+alert("Welcome to my About Me page!");
+console.log("Welcome alert has been shown.");
+
+// Paragraphs about myself
+document.write("<p>I am 16 years old and go to the Medina County Career Center. I've lived in Medina my entire life. I have two sisters, two dogs, and a cat. I work at Arby's and I play Soccer for the Medina High School.</p>")
+document.write("<p>I also work for a game on Roblox called TDS: Reanimated. I make the gameplay and balance the game for the players. I also am considered the best player, as I was the first to break many records in the game, which led to my position in balancing.")
+document.write("<p>I play other games, like Terraria and Geometry Dash. I don't go out much and prefer to stay at home. I also like to take pictures of my cat and send them to my friends. Everyone loves my cat and we nicknamed him after a boss from one of the games I play because he likes to bite me and the boss is also very aggressive, like him.</p>")
+
+// Log a message after the content has been successfully added to the page
+console.log("Dynamic content (name and paragraphs) has been written to the page.");
---
+++ js01/script.js
@@ -0,0 +1,2 @@
+console.log("External File Loaded");
+document.write("I learned how to do simple tasks in JavaScript like logging to the console and how to write with it.");
